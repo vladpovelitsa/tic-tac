5f1593d9b24f7f605d6b7cc7c69b453dac712173
vladpovelitsa <vlad.povelitsa@gmail.com>
2020-09-13 23:48:27 +0300

    Updates

---
+++ precache-manifest.6e0de6dac55558a22494284876adb2e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6de66f420817e9c81732e723b108fc",
+    "url": "/tic-tac/index.html"
+  },
+  {
+    "revision": "761011e705d08fc553bb",
+    "url": "/tic-tac/static/css/main.feb87780.chunk.css"
+  },
+  {
+    "revision": "210717bcd6136923a012",
+    "url": "/tic-tac/static/js/2.5ccea08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac/static/js/2.5ccea08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761011e705d08fc553bb",
+    "url": "/tic-tac/static/js/main.8067a2f4.chunk.js"
+  },
+  {
+    "revision": "0b78886d0935987a7810",
+    "url": "/tic-tac/static/js/runtime-main.2735cb23.js"
+  }
+]);
